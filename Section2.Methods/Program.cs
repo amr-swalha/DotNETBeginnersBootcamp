9270f784bc496b81c9b5bfdd9fd2ef4097f26e30
Amr Swalha <amr.swalha.code@outlook.com>
2018-04-22 20:06:03 +0300

    Methods demo

--- Section2.Methods/Program.cs
+++ Section2.Methods/Program.cs
@@ -10,6 +10,27 @@ namespace Section2.Methods
     {
         static void Main(string[] args)
         {
+            PrintMessages();
+            SayHello("Joe");
+            Console.WriteLine(CalculateReward(50));
+            Console.ReadLine();
+        }
+
+        public static void PrintMessages()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm a C# developer!");
+        }
+
+        public static void SayHello(string username)
+        {
+            Console.WriteLine($"Hello {username}");
+        }
+
+        public static int CalculateReward(int moneySpent)
+        {
+            int rewardPoints = moneySpent * 10;
+            return rewardPoints;
         }
     }
 }
