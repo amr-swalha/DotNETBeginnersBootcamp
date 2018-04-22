1016e27dbc10472b8601379b7f4249bc0d8fce55
Amr Swalha <amr.swalha.code@outlook.com>
2018-04-22 20:13:53 +0300

    if demo

--- Section2.SelectionStatement/Program.cs
+++ Section2.SelectionStatement/Program.cs
@@ -10,6 +10,14 @@ namespace Section2.SelectionStatement
     {
         static void Main(string[] args)
         {
+            int x = 6;
+            int y = 7;
+            if (x < y)
+            {
+                Console.WriteLine("Y greater than X");
+            }
+
+            Console.ReadLine();
         }
     }
 }
