94ebf435d19f3a8783c97751792e66f8c089676f
Amr Swalha <amr.swalha.code@outlook.com>
2018-04-23 21:21:37 +0300

    Adding if statement else example

--- Section2.SelectionStatement/Program.cs
+++ Section2.SelectionStatement/Program.cs
@@ -12,11 +12,29 @@ namespace Section2.SelectionStatement
         {
             int x = 6;
             int y = 7;
+            int z = 10;
+            int r = 11;
             if (x < y)
             {
                 Console.WriteLine("Y greater than X");
             }
 
+            if (x > y)
+            {
+                Console.WriteLine("X greater than Y");
+            }
+            else if (z > y)
+            {
+                Console.WriteLine("Z greater than Y");
+            }
+            else if (r > z)
+            {
+                Console.WriteLine("r greater than z");
+            }
+            else
+            {
+                Console.WriteLine("Y greater than X");
+            }
             Console.ReadLine();
         }
     }
