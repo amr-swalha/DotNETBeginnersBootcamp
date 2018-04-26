c87458dbf87ff872dbacab825ae6a9b3c4fdce40
Amr Swalha <amr.swalha.code@outlook.com>
2018-04-26 19:16:46 +0300

    iteration examples

--- Section3.IterationStatement/Program.cs
+++ Section3.IterationStatement/Program.cs
@@ -10,6 +10,7 @@ namespace Section3.IterationStatement
     {
         static void Main(string[] args)
         {
+            /*For iteration*/
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine($"The value of i is: {i}");
@@ -18,7 +19,27 @@ namespace Section3.IterationStatement
                     break;
                 }
             }
+            /*For iteration*/
+            /*While iteration*/
+            int z = 0;
+            while (z < 10)
+            {
+                Console.WriteLine(z);
+                if (z == 5)
+                {
+                    break;
+                }
+                z += 1;
+            }
+            /*While iteration*/
 
+            /*forEach iteration*/
+            string x = "Hello World!";
+            foreach (var letter in x)
+            {
+                Console.WriteLine(letter);
+            }
+            /*forEach iteration*/
             Console.ReadLine();
         }
     }
