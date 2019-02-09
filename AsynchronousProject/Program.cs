c339a17409d667935a71faedd0dd1b40678121a4
Amr Swalha <amr.swalha.code@outlook.com>
2019-02-09 17:12:50 +0400

    Async methods

---
+++ AsynchronousProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AsynchronousProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestAsync();
+            Console.ReadLine();
+        }
+        public static async void TestAsync()
+        {
+            HttpClient client = new HttpClient();
+            string getStringTask = await client.GetStringAsync("http://msdn.microsoft.com");
+            Console.WriteLine(getStringTask);
+        }
+    }
+}
