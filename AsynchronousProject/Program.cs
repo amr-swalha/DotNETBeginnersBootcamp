87d97b22ae6ff0577fd39d302999472c606602ec
Amr Swalha <amr.swalha.code@outlook.com>
2019-02-09 21:54:11 +0400

    adding async code

--- AsynchronousProject/Program.cs
+++ AsynchronousProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsynchronousProject
 {
@@ -13,8 +15,16 @@ namespace AsynchronousProject
         public static async void TestAsync()
         {
             HttpClient client = new HttpClient();
-            string getStringTask = await client.GetStringAsync("http://msdn.microsoft.com");
-            Console.WriteLine(getStringTask);
+            //string getStringTask = await client.GetStringAsync("http://msdn.microsoft.com");
+            await GetTask("Task method");
+        }
+        public static Task GetTask(string data)
+        {
+            Thread.Sleep(2000);
+            return Task.Run(() =>
+            {
+                Console.WriteLine(data);
+            });
         }
     }
 }
