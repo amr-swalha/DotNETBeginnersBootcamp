4d98e39bc2655e04ff0e8c247f632a7d052a00ab
Amr Swalha <amr.swalha.code@outlook.com>
2019-02-09 15:15:17 +0400

    Adding thread example

--- Section6.Delegates/Program.cs
+++ Section6.Delegates/Program.cs
@@ -8,8 +8,16 @@ namespace Section6.Delegates
 {
     class Program
     {
+        public delegate void Del(string message);
+        public static void DelMessage(string message) => Console.WriteLine($"The message: {message}");
+        public static void DelOtherMessage(string message) => Console.WriteLine($"The message from another: {message}");
         static void Main(string[] args)
         {
+            Del del = DelMessage;
+            del.Invoke("Hello Delegate");
+            del = DelOtherMessage;
+            del.Invoke("Another Message");
+            Console.ReadLine();
         }
     }
 }
---
+++ ThreadingProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingProject
+{
+    class Program
+    {
+        public static void ThreadProcess()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Thread Process {i}");
+                Thread.Sleep(0);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread threadProcess = new Thread(new ThreadStart(ThreadProcess));
+            threadProcess.IsBackground = true;                
+            threadProcess.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            Console.ReadLine();
+        }
+    }
+}
