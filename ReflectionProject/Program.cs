a3a99df7492c2c57ef73d904c5947571d51eb11f
Amr Swalha <amr.swalha.code@outlook.com>
2019-02-16 20:46:03 +0400

    reflection project

---
+++ ReflectionProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReflectionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Type type = student.GetType();
+            var methods = type.GetMethods();
+            var props = type.GetProperties();
+            foreach (var item in methods)
+            {
+                Console.WriteLine(item.Name);
+            }
+            foreach (var item in props)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public void GetName() => Console.WriteLine(Name);
+    }
+}
