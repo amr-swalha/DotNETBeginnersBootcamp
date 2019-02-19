d4fc3fb89c27de9d279a2dca677e0833eeb5c73d
Amr Swalha <amr.swalha.code@outlook.com>
2019-02-19 20:33:02 +0400

    Linq query

---
+++ LINQProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[5] { 5, 7, 9, 33, 22 };
+            var query = from x in data where x > 5 orderby x select x;
+            var fulent = data.Where(x => x > 5).OrderBy(x => x).Select(x => x);
+            foreach (var num in query)
+            {
+                Console.WriteLine($"num: {num}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
