60e0f6b801b28f92949b1af7e1c9aba1355b01f2
Amr Swalha <amr.swalha.code@outlook.com>
2019-02-19 20:50:41 +0400

    Where example

--- LINQProject/Program.cs
+++ LINQProject/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LINQProject
 {
@@ -10,13 +6,8 @@ namespace LINQProject
     {
         static void Main(string[] args)
         {
-            int[] data = new int[5] { 5, 7, 9, 33, 22 };
-            var query = from x in data where x > 5 orderby x select x;
-            var fulent = data.Where(x => x > 5).OrderBy(x => x).Select(x => x);
-            foreach (var num in query)
-            {
-                Console.WriteLine($"num: {num}");
-            }
+            WhereExample whereExample = new WhereExample();
+            whereExample.SimpleWhere();
             Console.ReadLine();
         }
     }
---
+++ LINQProject/WhereExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQProject
+{
+    public class WhereExample
+    {
+        int[] dataSource = new int[10] { 2, 9, 8, 7, 14, 5, 99, 102, 587, 16 };
+        public void SimpleWhere()
+        {
+            var query = from x in dataSource where IsMatched(x) select x;
+            Console.WriteLine("=====SimpleWhere=====");
+            Console.WriteLine($"Element Count {query.Count()}");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("=====SimpleWhere=====");
+        }
+        public bool IsMatched(int value)
+        {
+            return value > 10 || value < 100;
+        }
+    }
+}
